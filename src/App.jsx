2db986edcd81e9cf2ea5741b95f805892c0473b1
Tushar Mahat <tusharmahat@gmail.com>
2024-07-21 01:04:52 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero title="Test title" subtitle="This is a subtitle" />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  )
+}
+
+export default App
